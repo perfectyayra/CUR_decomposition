206f9b3bd0e72a8fc46c4a73716657f10fb7cb5e
Perfect Yayra Gidisu <36855498+perfectyayra@users.noreply.github.com>
2022-01-17 11:20:04 +0100

    Update cur_deim.m

--- cur_deim.m
+++ cur_deim.m
@@ -1,4 +1,4 @@
-function [irow, icol, U] = cur_deim(A, k)
+function [C, U, R] = cur_deim(A, k)
 
 %CUR_DEIM  DEIM incurred CUR decomposition
 % function [icol, irow, M] = cur_deim(A, m)
@@ -11,17 +11,15 @@ function [irow, icol, U] = cur_deim(A, k)
 
 if nargin < 2 || isempty(k), k = 2; end
 
-[U, ~, V] = svd(full(A),0);
-U = U(:,1:k);
-V = V(:,1:k);
-
-
+[U, ~, V] = svds(full(A),k);
 
 for i = 1:k
   [~, irow(i)] = max(abs(U(:,i)));
   [~, icol(i)] = max(abs(V(:,i)));
-  U(:,i+1:end) = U(:,i+1:end) - U(:,1:i) * (U(irow,1:i) \ U(irow,i+1:end));
-  V(:,i+1:end) = V(:,i+1:end) - V(:,1:i) * (V(icol,1:i) \ V(icol,i+1:end));
+  U(:,i+1:k) = U(:,i+1:k) - U(:,1:i) * (U(irow,1:i) \ U(irow,i+1:k));
+  V(:,i+1:k) = V(:,i+1:k) - V(:,1:i) * (V(icol,1:i) \ V(icol,i+1:k));
 end
-U = A(:,icol) \ (A / A(irow,:));
+C=A(:,icol);
+R=A(irow,:);
+U = C \ (A / R);
 
