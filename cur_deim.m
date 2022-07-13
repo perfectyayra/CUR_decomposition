2aaef7ea1c920147a798223e7a9aad8fee03c52a
Perfect Yayra Gidisu <36855498+perfectyayra@users.noreply.github.com>
2022-07-13 11:01:10 +0200

    Update cur_deim.m

--- cur_deim.m
+++ cur_deim.m
@@ -14,11 +14,15 @@ if nargin < 2 || isempty(k), k = 2; end
 
 [U, ~, V] = svds(A,k);
 
-for i = 1:k
-  [~, irow(i)] = max(abs(U(:,i)));
-  [~, icol(i)] = max(abs(V(:,i)));
-  U(:,i+1:k) = U(:,i+1:k) - U(:,1:i) * (U(irow,1:i) \ U(irow,i+1:k));
-  V(:,i+1:k) = V(:,i+1:k) - V(:,1:i) * (V(icol,1:i) \ V(icol,i+1:k));
+irow=zeros(1,k);
+icol=zeros(1,k);
+for j = 1:k
+  [~, irow(j)] = max(abs(U(:,j)));
+  [~, icol(j)] = max(abs(V(:,j)));
+  if j<k
+   U(:,j+1) = U(:,j+1) - U(:,1:j) * (U(irow(1:j),1:j) \ U(irow(1:j),j+1));
+   V(:,j+1) = V(:,j+1) - V(:,1:j) * (V(icol(1:j),1:j) \ V(icol(1:j),j+1));
+  end
 end
 C=A(:,icol);
 R=A(irow,:);
